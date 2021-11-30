bb4f17d2e1e0d7be4e4cb023e584d6da8a2ee7e4
Caspar Meijer <capsar.meijer@gmail.com>
2021-11-30 22:50:53 +0100

    Improved rl_helper & included jensenshannon metric
    Also ignore Acrobot Q_Table

--- main.py
+++ main.py
@@ -1,69 +1,58 @@
-import sklearn
 import tensorflow as tf
 from tensorflow import keras
 import gym
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.linear_model import LogisticRegression
+from rl_helper import QLearning, DecisionTree
+from scipy.spatial import distance
 
-from rl_helper import QLearning
 
 print("Tensforflow version:", tf.__version__)
 print('keras version:', keras.__version__)
 
-env = gym.make("Acrobot-v1")
+env, discretize = gym.make("Acrobot-v1"), [5, 5, 5, 5, 2, 2]
+# env = gym.make("CartPole-v1")
 # env = gym.make("MountainCar-v0")
 print('Action space: ', env.action_space)
 print('Observation space:', env.observation_space)
 print('Metadata: ', env.metadata)
+print('')
 
 # Setup Qlearning to create expert model
-
-q_learner = QLearning(env, discretize=[5, 5, 5, 5, 2, 2])
-try:
-    q_learner.load()
-except:
-    print('file not found.')
-q_learner.train(50000, lr=0.5, epsilon=0.5, discount=0.99)
+q_learner = QLearning(env, discretize)
+q_learner.load()
+q_learner.train(1000, lr=0.5, epsilon=0.05, discount=0.99)
 q_learner.save()
 
 # Generate expert data from expert model
-q_agent = QLearning(env, discretize=[5, 5, 5, 5, 2, 2])
+q_agent = QLearning(env, discretize)
 q_agent.load()
-# q_agent.generate_trajectories(1, render=True)
+q_agent.get_average_reward(1, render=True)
 expert_trajectories = q_agent.generate_trajectories(200)
 expert_state_actions = [tuple(list(s) + [a]) for s,a in expert_trajectories]
 expert_labels = [1 for i in expert_trajectories]
-print(len(expert_trajectories), expert_trajectories[0:5], expert_state_actions[0:5], expert_labels[0:5])
 
 # Setup gail from expert data with decision tree as generative model and mlp as descriminative model
-epochs = 10
-max_sample_size = 5000
+epochs = 3
+max_sample_size = 1000
 number_of_tests = 50
-print('------------------------------------------------------------------------------')
+
 #init generator and discriminator policy parameters theta & w
-generator = DecisionTreeClassifier()
-generator.fit([env.reset()], [0])
+generator = DecisionTree(env, max_depth=3, max_features=2)
 
 discriminator = LogisticRegression()
 discriminator.fit([tuple(list(env.reset()) + [0]), tuple(list(env.reset()) + [1])], [0, 1])
 
-for i in range(epochs):
-    total_reward = 0
-    for ii in range(number_of_tests):
-        state = env.reset()
-        done = False
-        while done == False:
-            action = generator.predict([state])[0]
-            state, reward, done, info = env.step(action)
-            total_reward+=reward
-    print('Total average reward:', total_reward / number_of_tests)
+generator.get_average_reward(number_of_tests)
+print('------------------------------------------------------------------------------')
 
+for i in range(epochs):
+
+    print('Epoch: ', i)
     #Generate state action pairs from generator (expert states used as input)
-    generator_state_actions = [tuple(list(s) + list(generator.predict([s]))) for s,_ in list(expert_trajectories)]
+    generator_state_actions = [tuple(list(s) + [generator.do_action(s)]) for s,_ in list(expert_trajectories)]
     generator_labels = [0 for i in generator_state_actions]
     # print(generator_trajectories[0:5], generator_labels[0:5])
-
 
     sample_state_actions, sample_labels = [], []
     sample_state_actions.extend(expert_state_actions)
@@ -73,15 +62,16 @@ for i in range(epochs):
     # print(len(sample_state_actions), len(sample_labels), len(expert_state_actions), len(expert_labels), len(generator_state_actions), len(generator_labels))
     # print(list(zip(sample_state_actions, sample_labels))[0:5], list(zip(expert_state_actions, expert_labels))[0:5], list(zip(generator_state_actions, generator_labels))[0:5])
 
-
     #Train the discriminator with full sample trajectories and labels.
     discriminator.fit(sample_state_actions, sample_labels)
     print('Discriminator score on expert & generator data:', discriminator.score(expert_state_actions, expert_labels), discriminator.score(generator_state_actions, generator_labels))
     print('Discriminator prediction on expert & generator data:', np.array(discriminator.predict(expert_state_actions)).mean(), np.array(discriminator.predict(generator_state_actions)).mean())
-
+    print('Discriminator prediction probability on expert & generator data:', np.array(discriminator.predict_proba(expert_state_actions))[:,1].mean(), np.array(discriminator.predict_proba(generator_state_actions))[:,1].mean())
+    print('The Jensen-Shannon distance between expert and generator data:', distance.jensenshannon([a[-1] for a in expert_state_actions], [a[-1] for a in generator_state_actions]))
+    
     #Sample random amount of state_action and labels.
-    idx = np.random.choice(len(sample_state_actions), size=min(len(sample_state_actions), max_sample_size))
-    sample_state_actions, sample_labels = [sample_state_actions[i] for i in idx], [sample_labels[i] for i in idx]
+    # idx = np.random.choice(len(sample_state_actions), size=min(len(sample_state_actions), max_sample_size))
+    # sample_state_actions, sample_labels = [sample_state_actions[i] for i in idx], [sample_labels[i] for i in idx]
 
     #Get all state action pairs classified as expert data by the discriminator
     new_generator_s, new_generator_a = [], []
@@ -91,17 +81,17 @@ for i in range(epochs):
             new_generator_a.append(s_a[-1])
     generator.fit(new_generator_s, new_generator_a)
 
-total_reward = 0
-for ii in range(3):
-    state = env.reset()
-    done = False
-    while done == False:
-        env.render()
-        action = generator.predict([state])[0]
-        state, reward, done, info = env.step(action)
-        total_reward+=reward
-print('Total average reward:', total_reward / 3)
+    #Get average reward of the generator.
+    generator.get_average_reward(number_of_tests)
 
+    print('')
+
+
+
+#Final test expert vs Generator:
+total_tests = 500
+q_agent.get_average_reward(total_tests)
+generator.get_average_reward(total_tests)
 
 # Evaluate the new generative model in terms of interpretability (size, average path length, compared to optimal)
 
--- rl_helper.py
+++ rl_helper.py
@@ -2,28 +2,95 @@ import numpy as np
 from gym import Env
 import pickle
 from numpy.core.fromnumeric import size
-
 from typing import Any
+from sklearn.tree import DecisionTreeClassifier
 
 class RL_Algorithm():
-    def __init__(self):
+    def __init__(self, env: Env):
         pass
 
     def loadz(self, file: str) -> Any:
-        with open(file, 'rb') as f:
-            return pickle.load(f)
+        try:
+            with open(file, 'rb') as f:
+                return pickle.load(f)
+        except:
+            print('file not found:', file)
 
     def savez(self, obj: Any, file: str):
         with open(file, 'wb') as f:
             pickle.dump(obj, f)
 
-        
+    def generate_trajectories(self, do_action, env:Env, n=10, render=False):
+        trajectories = []
 
+        for i in range(n):
+            #Reset Environmnet
+            state = env.reset()
+            done = False
+            while done == False:
+                if render:
+                    env.render()
+                action = do_action(state)
+
+                #perform action in environment
+                next_state, reward, done, info = env.step(action)
+        
+                #Save state and action to memory
+                trajectories.append((state, action))
+
+                state = next_state
+                
+            env.close()
+            #save trajectory and actions to large memory
+        return trajectories
+
+    def get_average_reward(self, do_action, env:Env, number_of_tests, actor, render=False):
+        total_reward = 0
+        for ii in range(number_of_tests):
+            state = env.reset()
+            done = False
+            while done == False:
+                if render:
+                    env.render()
+                action = do_action(state)
+                state, reward, done, info = env.step(action)
+                total_reward+=reward
+        if number_of_tests == 1:
+            print(f'Total average {actor} reward: {total_reward / number_of_tests}')
+        else:
+            print(f'Total average {actor} reward over {number_of_tests} games is: {total_reward / number_of_tests}')
+        return total_reward
+
+class DecisionTree(RL_Algorithm):
+
+    def __init__(self, env: Env, max_depth=5, max_features=2):
+        self.env = env
+        self.env.reset()
+        self.max_depth = max_depth
+        self.max_features = max_features
+        self.decision_tree = DecisionTreeClassifier(max_depth=max_depth, max_features=max_features)
+        self.decision_tree.fit([env.reset()], [-1])
+
+    def load(self):
+        self.decision_tree = self.loadz(f'./models/DecisionTree_{self.env.spec.id}_{self.max_depth}_{self.max_features}')
+
+    def save(self):
+        self.savez(self.decision_tree, f'./models/DecisionTree_{self.env.spec.id}_{self.max_depth}_{self.max_features}')
+
+    def do_action(self, state):
+        return self.decision_tree.predict([state])[0]
+
+    def fit(self, new_generator_s, new_generator_a):
+        self.decision_tree.fit(new_generator_s, new_generator_a)
+
+    def generate_trajectories(self, n=10, render=False):
+        super().generate_trajectories(self.do_action, self.env, n, render)
+
+    def get_average_reward(self, number_of_tests, render=False):
+        return super().get_average_reward(self.do_action, self.env, number_of_tests, "DecisionTree", render=False)
 
 class QLearning(RL_Algorithm):
 
-    #TODO: Make size of Q_table dependable on observation_space.
-    #TODO: Setup Q_table according to size not upscaling and rounding.
     def __init__(self, env: Env, discretize: list):
         self.env = env
         self.env.reset()
@@ -46,35 +113,20 @@ class QLearning(RL_Algorithm):
             self.Q_table[tuple(state_adj)] = np.random.uniform(low=-1.0, high=1.0, size=self.env.action_space.n)
         return state_adj
 
-    def do_action(self, state_adj, epsilon=0):
+    def do_action(self, state, epsilon=0):
         if np.random.random() > epsilon:
-            return np.argmax(self.Q_table[tuple(state_adj)])
+            return np.argmax(self.Q_table[tuple(self.discretizeState(state))])
         else:
-            return self.env.action_space.sample()   
+            return self.env.action_space.sample() 
+
+    def predict_q(self, state):
+        return np.max(self.Q_table[tuple(self.discretizeState(state))])
 
     def generate_trajectories(self, n=10, render=False):
-        trajectories = []
+        return super().generate_trajectories(self.do_action, self.env, n, render)
 
-        for i in range(n):
-            #Reset Environmnet
-            state = self.env.reset()
-            done = False
-            while done == False:
-                if render:
-                    self.env.render()
-                action = self.do_action(self.discretizeState(state))
-
-                #perform action in environment
-                next_state, reward, done, info = self.env.step(action)
-        
-                #Save state and action to memory
-                trajectories.append((state, action))
-
-                state = next_state
-
-            self.env.close()
-            #save trajectory and actions to large memory
-        return trajectories
+    def get_average_reward(self, number_of_tests, render=False):
+        return super().get_average_reward(self.do_action, self.env, number_of_tests, "Q_Learning", render=False)
 
     def train(self, epochs, lr, epsilon, discount):
         reward_list = []
@@ -87,21 +139,18 @@ class QLearning(RL_Algorithm):
             done = False
             tot_reward, reward = 0, 0
             state = self.env.reset()
-            state_adj = self.discretizeState(state)
-
             while done != True:
                 # Randomly do a random action, this will increase exploration in the beginning.
-                action = self.do_action(state_adj, epsilon=epsilon)
+                action = self.do_action(state, epsilon=epsilon)
 
                 state2, reward, done, info = self.env.step(action)
-                state2_adj = self.discretizeState(state2)
 
-                delta = lr*(reward + discount*np.max(self.Q_table[tuple(state2_adj)]) - self.Q_table[tuple(state_adj)][action])
-                self.Q_table[tuple(state_adj)][action] += delta
+                delta = lr*(reward + discount*np.max(self.Q_table[tuple(self.discretizeState(state2))]) - self.Q_table[tuple(self.discretizeState(state))][action])
+                self.Q_table[tuple(self.discretizeState(state))][action] += delta
 
                 #Update total_reward & update state for new action.
                 tot_reward += reward
-                state_adj = state2_adj
+                state = state2
 
             # Decay epsilon
             if epsilon > 0:
